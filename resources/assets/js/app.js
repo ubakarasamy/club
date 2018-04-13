b117aa9b29cf73ff9226c3f20921d08a6d1d2f71
ubakarasamy <smuba4@gmail.com>
2018-04-13 15:01:08 +0530

    project configured and navbar designed

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,3 +9,15 @@ import Buefy from 'buefy';
 Vue.use(Buefy);
 
 // Vue.component('example', require('./components/Example.vue'));
+
+
+var app = new Vue({
+    el: '#app',
+    data: {}
+});
+
+$(document).ready(function(){
+    $('button.dropdown').hover(function(e){
+    $(this).toggleClass('is-open');
+});
+});
